b7715cb87d85e2de50ae714d43118682ed56650b
Gregory Brown <gregorybrown@gregorys-mbp.attlocal.net>
2017-11-20 19:10:00 -0500

    HM 1st commit

---
+++ assets/Javascript/game.js
@@ -0,0 +1,44 @@
+// Hangman Game
+
+var word ="madonna"
+
+// Use Key Elements for the Letters the players will type 
+// Use onkeyup event listening
+
+// Press any key to get started 
+
+// Log your wins. Number of times users get the word right.
+//Create a variable for wins,
+//Set the variable to 0.
+var wins = 0;
+// If aa player wins, Incrase by 11.
+var playerWins =function () {
+	wins++;
+	// body...
+} 
+
+// Number of guesses remaining for the user
+// Create a variable for the user.
+// Set the variable 12(optional)
+// If the player misses a letter decrease by 1.
+
+
+// Display letters users guesses
+
+
+
+
+// Create a variable for the word
+// Display a dash , for each latter in the word.
+// use .lenth to chech the length of the word.
+// Create a for loop that puts as many dashes as there are letters.
+$(document).ready(function(){
+
+
+})
+for (var i = 0; i < word.length; i++) {
+	console.log("The loop ran") 
+	$( "#word-one" ).append( "<span> _ </span>" );
+	// Append a new ____ space evert time the loop runs.
+
+}
