1f552260cc054da18759245d0eacb31ae4848cf7
Gregory Brown <gregorybrown@gregorys-mbp.attlocal.net>
2017-11-20 19:52:11 -0500

    update game

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -2,8 +2,14 @@
 
 var word ="madonna"
 
-// Use Key Elements for the Letters the players will type 
+// Use Key event for the Letters the players will type 
 // Use onkeyup event listening
+// Create variable for currentKeyPressed
+
+// Create a loop that looks through an array
+// Compare the currentKeyPressed and see if it equals the letter at that index
+//If it matches- display it.
+// If it doesn't match add it to letters guessed.
 
 // Press any key to get started 
 
@@ -24,8 +30,13 @@ var playerWins =function () {
 
 
 // Display letters users guesses
+// get Element by ID
+// append to the element
 
+var lettersGuessed = function(userKeyPressed) {
+	$("#letters-guessed").append("<span>" + userKeyPressed + "</span>");
 
+}
 
 
 // Create a variable for the word
@@ -33,12 +44,12 @@ var playerWins =function () {
 // use .lenth to chech the length of the word.
 // Create a for loop that puts as many dashes as there are letters.
 $(document).ready(function(){
-
-
-})
-for (var i = 0; i < word.length; i++) {
-	console.log("The loop ran") 
+	for (var i = 0; i < word.length; i++) {
+		console.log("The loop ran") 
 	$( "#word-one" ).append( "<span> _ </span>" );
 	// Append a new ____ space evert time the loop runs.
 
 }
+
+
+})
